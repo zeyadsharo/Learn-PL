f038f1edec6674d5873a7e841645653f8790f52a
zeyadsharo <33178745+zeyadsharo@users.noreply.github.com>
2020-05-04 16:01:13 +0300

    remove an needed file

--- mobile_application/lib/main.dart
+++ mobile_application/lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_application/class/getdata.dart';
-import 'package:mobile_application/class/savedata.dart';
-import 'package:mobile_application/utils/database_helper.dart';
-import 'model/courseconsepts.dart';
-import 'model/lang.dart';
-import 'model/objects.dart';
 import 'ui/StartScreen.dart';
 
 List myUsers;
